1c0692a06ff472ef12c8ee6cabdb60e7cebdec5d
rotemmayo <rotem.mayo@gmail.com>
2020-04-30 20:53:05 +0300

    Add get_nsubjettiness to parser

--- event_file_parser.py
+++ event_file_parser.py
@@ -137,5 +137,42 @@ class EventFileParser:
         """
         return EventFileParser.m1(jets)-EventFileParser.m2(jets)
 
+    @staticmethod
+    def get_nsubjettiness(jet, R=1.0, max_tau=4):
+        """
+        Calculate tau1, ..., tauN for the given jet (where N = max_tau)
+
+        :param jet: The jet
+        :param R: Radius to use when clustering
+        :param max_tau: Highest tau to calculate
+        :return: List containing all taus
+        """
+        particles = jet.constituents_array()
+        num_particles = len(particles['eta'])
+        sequence = cluster(particles, R=R, p=1)
+
+        tau = np.zeros(max_tau)
+
+        for i in range(1, min(max_tau + 1, num_particles)):
+            sub_jets = sequence.exclusive_jets(i)
+
+            delta_rs = np.zeros((num_particles, i))
+            for j, sub_jet in enumerate(sub_jets):
+
+                eta = particles['eta']
+                phi = particles['phi']
+                pi = np.pi
+
+                if abs(sub_jet.phi) > pi / 2:
+                    delta_rs[:, j] = np.sqrt(
+                        (eta - sub_jet.eta) ** 2 + (phi % (2 * pi) - sub_jet.phi % (2 * pi)) ** 2)
+                else:
+                    delta_rs[:, j] = np.sqrt((eta - sub_jet.eta) ** 2 + (phi - sub_jet.phi) ** 2)
+
+            delta_r = np.min(delta_rs, axis=1)
+            tau[i - 1] = 1. / np.sum(particles['pT'] * R) * np.sum(particles['pT'] * delta_r)
+
+        return list(tau)
+
     # TODO: Add n-subjettiness of different levels. Add more relevant observables.
     # TODO: Add output_h5_file function.
