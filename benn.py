1710f315561d4554f81b406ecb0d9a91c8662b0b
Rotem Ovadia <rotemov@gmail.com>
2021-01-05 20:02:48 +0200

    maximized resources over yonitq, fixed benn

--- benn.py
+++ benn.py
@@ -76,6 +76,7 @@ def create_partial_data_set(full_data_name, data_path, experiment_name, obs_list
 def main():
     R = 1.0
     p = EventFileParser(TRAINING_DATA_FILE_PATH, CSV_FILE_PATH.format(R), R=R)
+    p.parse()
     # create_full_data(DATA_PATH)  # need to shuffle jets and partons for supervised training
     """
     for i in range(len(EXPERIMENT_NAMES)):
